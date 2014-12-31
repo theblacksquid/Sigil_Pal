ee50ff120324bd2cfb642811f31604a714eb996b
Aeron Paul Sioson <theblacksquid@users.noreply.github.com>
2014-12-31 09:31:19 +0800

    corrected getBaseNum()

--- js/SigilPal.js
+++ js/SigilPal.js
@@ -1,83 +1,82 @@
 
 /*
-    SigilPal.js
-    This is a JavaScript implementation of my earlier 
-    SigilPal.py program. This contains the back-end logic 
-    that will be rendered on screen by the included webApp
+ * SigilPal.js is a JavaScript implementation of my SigilPal.py
+ * program that turns a statement of intent into a string of letters
+ * and provide numerological values to it for sigilwork.
+ */
+ 
+numerology = {
+	"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,
+	"J":10,"K":20,"L":30,"M":40,"N":50,"O":60,"P":70,"Q":80,"R":90,
+	"S":100,"T":200,"U":300,"V":400,"W":500,"X":600,"Y":700,"Z":800,
+	} 
 
-*/
+function getNumList(string) {
+	var index = 0; result = [];			// turns a string value into
+	var str = string.toUpperCase()		// an array of its numerical
+	for (var char in str) {			// equivalents based on object
+		if (str[index] === " ") {	// numerology
+			index = index + 1;
+			continue;
+			}
+			
+		else {
+			var i = str[index];
+			var num = numerology[i];
+			result.push(num);
+			index = index + 1;
+			}
+		};
+	
+	return result;
+	}
 
-var numerology = {
-"A":1,"B":2,"C":3,"D":4,"E":5,"F":6,"G":7,"H":8,"I":9,
-"J":10,"K":20,"L":30,"M":40,"N":50,"O":60,"P":70,"Q":80,"R":90,
-"S":100,"T":200,"U":300,"V":400,"W":500,"X":600,"Y":700,"Z":800,
-    }
-
-
-function getNumList(string_val) {
-    var i = 0; result = [];
-    var str = string_val.toUpperCase();
-    while (i < string_val.length) {
-        var x = str[i];
-        if (x === " ") {
-            i = i + 1;
-            continue;
-            }
-        else {
-            result.push(numerology[x]);
-            i = i + 1;
-            }
-        };
-    return result;
-    }
-
-function getNumVal(int_list) {
-    var result = 0;
-    for (var i in int_list) {
-        result = result + int_list[i];
-        };
-    return result;
-    }
-
+function getNumVal(numArray) {
+	var result = 0;
+	for (var i in numArray) {
+		result = result + numArray[i];
+		};
+	return result;
+	}
+	
 function getBaseNum(int_val) {
-    var str = String(int_val);
-    var list = []; i = 0;
-    var result =0;
-    while (str.length > 1) {
-        while (i < str.length) {
-            list.push(Number(str[i]));
-            i = i + 1;
-            };
-        for (var x in list) {
-            result = result + list[x];
-            };
-        str = Number(result);
-        }
-    return result;
-    }
-
+	var string = String(int_val);
+	while (string.length != 1) {
+		var result = 0;
+		for (var i in string) {
+			result = result + Number(string[i])
+			};
+		string = String(result);
+		};
+	return result;
+	}
+	
 function check(item, list) {
-    for (var i in list) {
-        if (list[i] === item) {return true;}
-        };
-    return false;
-    }
+	for (var i in list) {
+		var n = list[i];
+		if (n === item) {
+			return true;
+			}
+		};
+	return false;
+	}
 
-function xtract_letters(str_val) {
-    var result = ""; i= 0; already = [];
-    var raw = str_val.toUpperCase();
-    while (i < raw.length) {
-        var x = raw[i];
-        var isAlready = check(x, already);
-        if (isAlready === true) {
-            i = i + 1;
-            continue;
-            }
-        else {
-            already.push(x);
-            result = result + x;
-            i = i + 1;
-            }
-        };
-    return result;
-    }
+function xtract_letters(string) {
+	var result = ""; already = [];
+	for (var i in string) {
+		var letter = string[i].toUpperCase();
+		if (check(letter, already) == true) {
+			continue;
+			}
+		else {
+			already.push(letter);
+			result = result + letter
+			}
+		};
+	return result;
+	}
+	
+
+	
+	
+	
